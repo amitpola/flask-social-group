43d4cd5ecd3eb8f0a2fe4e8dbc86add0beb7aba4
amitpola <amitpola000@gmail.com>
2023-03-06 17:48:43 +0530

    DELETE method of pymongo implemented

--- resources/group.py
+++ resources/group.py
@@ -1,8 +1,8 @@
 from bson import ObjectId
-from flask import request,Response, jsonify
+from flask import request,Response
 from flask_restful import Resource
 from database.models import Group, User
-from flask_jwt_extended import jwt_required, current_user, get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from utils import messages
 from database.db import social_group_instance
 import json
@@ -19,7 +19,7 @@ class GroupsApi(Resource):
         user_id = get_jwt_identity()
         loggedinUser = User.objects.get(id=user_id)
         user_email = loggedinUser.email
-        
+
         body = request.get_json()
 
         group_col_instance = social_group_instance['group']
@@ -90,19 +90,31 @@ class MakeOrRemoveAdmin(Resource):
         loggedinUser = User.objects.get(id=user_id)
         user_email = str(loggedinUser.email)
         group = Group.objects.get(id=id)
-
-        if group.admins.count(user_email) != 0:
-            if group.admins.count(member_to_remove) != 0:
-                group.admins.remove(member_to_remove)
-            if group.moderators.count(member_to_remove) != 0:
-                group.moderators.remove(member_to_remove)
-            if group.members.count(member_to_remove) != 0:
-                group.members.remove(member_to_remove)
-            
-            group.save()
-            
-            return messages.success_message, 200
         
+        document = social_group_instance.group.find_one({"_id":ObjectId(id)})
+
+        if document:
+            admins = document['admins']
+            moderators = document['moderators']
+            members = document['members']
+
+            if admins.count(user_email) != 0:
+                if moderators.count(member_to_remove) != 0:
+                    moderators.remove(member_to_remove)
+                if members.count(member_to_remove) != 0:
+                    members.remove(member_to_remove)
+
+            elif moderators.count(user_email) != 0:
+                if members.count(member_to_remove) != 0:
+                    members.remove(member_to_remove)
+
+            elif members.count(user_email) != 0:
+                members.remove(member_to_remove)            
+                
+        
+            social_group_instance.group.update_one({'_id': document['_id']}, {"$set": document})
+            return messages.success_message, 200
+
         return messages.error_message, 401 
 
 
